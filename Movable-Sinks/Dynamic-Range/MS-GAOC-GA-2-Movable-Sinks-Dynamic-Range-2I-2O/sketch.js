345cbf92868de3100430308219eafb9e1b03c7b3
csprojectlab <aridaman.nandan@outlook.com>
2020-02-27 11:36:37 +0530

    optigachs-moms-in&out-asr update

--- Movable-Sinks/Dynamic-Range/MS-GAOC-GA-2-Movable-Sinks-Dynamic-Range-2I-2O/sketch.js
+++ Movable-Sinks/Dynamic-Range/MS-GAOC-GA-2-Movable-Sinks-Dynamic-Range-2I-2O/sketch.js
@@ -41,7 +41,7 @@ let pop = null;
 let iterations = 30;
 let it = 0;
 let deploymentStrategy = true;
-let tier = "Tier 3";
+let tier = "Tier 1";
 const Tier = {
     "T1": "Tier 1", "T2": "Tier 2", "T3": "Tier 3"
 }
@@ -131,7 +131,7 @@ function energyModel () {
             deadCount = currentDeadCount;
             console.log("Rounds: ", r, "Dead Nodes: ", deadCount, "Energy: ", network.calNetEnergy());
             storeResult (r, deadCount, network.calNetEnergy(), pop.chromosomes[pop.fittestIndex].countClusterHeads(), RadioConsumptionModel.dataPacketSent, d.sinksLoad)
-            RadioConsumptionModel.nprob += 0.01;
+            RadioConsumptionModel.nprob += 0.05;
             RadioConsumptionModel.cprob += 0.04;
             break;
         }

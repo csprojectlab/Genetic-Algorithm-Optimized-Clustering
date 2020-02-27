15550eba713b1866bff1bdff5a3b2de92b8c7d35
csprojectlab <aridaman.nandan@outlook.com>
2020-02-27 15:34:00 +0530

    changes for result

--- MS-GAOC/MS-GAOC-Dynamic-Range/classes/dissipate.js
+++ MS-GAOC/MS-GAOC-Dynamic-Range/classes/dissipate.js
@@ -1,5 +1,5 @@
 class RadioConsumptionModel {
-    static cprob = 0.27;
+    static cprob = 0.35;
     static nprob = 0.15;
     static dataPacketSent = 0;
     constructor () {
@@ -11,8 +11,8 @@ class RadioConsumptionModel {
     // TODO(Ari): Not discussed in MS-GAOC paper but will use if for enhancement
     broadcastMessage (clusters) {
         Object.keys(clusters).forEach (h_index => {
-            if (random(1) < 0.7)
-                RadioConsumptionModel.dataPacketSent++;
+            // if (random(1) < 0.7)
+            //     RadioConsumptionModel.dataPacketSent++;
          })
     }
 
